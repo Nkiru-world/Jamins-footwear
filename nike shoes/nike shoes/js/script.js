d24421bfed5a32d3ad72bfc39f5c34bec07ca871
Nkiru <108672024+Nkiru-world@users.noreply.github.com>
2023-03-23 13:46:48 +0100

    Add files via upload

---
+++ nike shoes/nike shoes/js/script.js
@@ -0,0 +1,56 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+  document.body.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+  if(window.innerWidth < 1200){
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+    document.body.classList.remove('active');
+  };
+};
+
+
+
+
+
+var swiper = new Swiper(".products-slider", {
+  loop:true,
+  spaceBetween: 20,
+  grabCursor:true,
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  loop:true,
+  spaceBetween: 20,
+  grabCursor:true,
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
